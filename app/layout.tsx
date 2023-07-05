1c829b49704dba85ad54db7246e594e802ff23c6
Saman <samangithoseinpour@gmail.com>
2023-07-05 13:06:04 +0330

    feat: navbar section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import '../public/globals.css';
+import { Navbar, Footer } from '@/components';
 
 export const metadata = {
   title: 'Cars-Gateway',
-  description: 'Discover, explore, rent cars',
+  description: 'Discover the best cars in the wolrd.',
 };
 
 const Root = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 };
